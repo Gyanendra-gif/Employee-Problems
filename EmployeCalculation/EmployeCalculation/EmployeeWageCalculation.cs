9be8f89b594e7ec1e0772b31a0b3e244c1113e58
Gyanendra Pratap Singh Sengar <[gyanendra1252@gmail.com]>
2021-11-03 16:11:32 +0530

    [Gyanendra] Add: Program to check Monthaly Wage of Employee

--- EmployeCalculation/EmployeCalculation/EmployeeWageCalculation.cs
+++ EmployeCalculation/EmployeCalculation/EmployeeWageCalculation.cs
@@ -6,39 +6,29 @@ namespace EmployeCalculation
 {
     class EmployeeWageCalculation
     {
-        public const int WAGE_PR_HR = 20, FULL_DAY_HOUR = 8, HALF_DAY_HOUR = 4, Present = 1, HalfDay = 2;
-        int totalSalary;
-        public void EmployeeDailyWag()
+        public const int WAGE_PR_HR = 20, FULL_DAY_HOUR = 8, HALF_DAY_HOUR = 4, Present = 1, HalfDay = 2, totalDays = 20;
+        int totalSalary, empHr;
+        public void MonthlyEmployeeWage()
         {
             Random rand = new Random();
             int empCheck = rand.Next(0, 3);
-            switch (empCheck)
-            {
-                case Present: totalSalary = WAGE_PR_HR * FULL_DAY_HOUR;
-                    Console.WriteLine("Employee is Present and salary is: "+totalSalary);
-                    break;
-                case HalfDay: totalSalary = WAGE_PR_HR * HALF_DAY_HOUR;
-                    Console.WriteLine("Employee is Present For HalfDay and salary is: " + totalSalary);
-                    break;
-                default:
-                    Console.WriteLine("Employee is Absent for the Day");
-                    break;
+            for (int i = 0; i < totalDays; i++)
+                switch (empCheck)
+                {
+                    case Present:
+                        this.empHr += FULL_DAY_HOUR;
+                        break;
+                    case HalfDay:
+                        this.empHr += HALF_DAY_HOUR;
+                        break;
+                    default:
+                        Console.WriteLine("Employee is Absent for the Day");
+                        break;
+                }
 
-            }
+            this.totalSalary = WAGE_PR_HR * empHr;
+            Console.WriteLine("Here is the Monthly Salary of Emp." + this.totalSalary);
 
-            /*if (empCheck == 1)
-            {
-                this.totalSalary = WAGE_PR_HR * FULL_DAY_HOUR;
-                Console.WriteLine("Employee is Present and Salary is: " + this.totalSalary);
-            }
-            if (empCheck == 2) 
-            {
-                this.totalSalary = WAGE_PR_HR * HALF_DAY_HOUR;
-                Console.WriteLine("Employee is Present for Half Day and Salary is: " + this.totalSalary);
-            }
-
-            else
-                Console.WriteLine("Employee is Absent");*/
         }
     }
 }
