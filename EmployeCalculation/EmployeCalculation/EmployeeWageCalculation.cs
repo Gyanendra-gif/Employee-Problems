9e282d00d69edd9d78aeb5ad654a2ed4bd3d4b53
Gyanendra Pratap Singh Sengar <[gyanendra1252@gmail.com]>
2021-11-03 15:47:18 +0530

    [Gyanendra] Add: Program to Check Emp. Wage using Switch-Case

--- EmployeCalculation/EmployeCalculation/EmployeeWageCalculation.cs
+++ EmployeCalculation/EmployeCalculation/EmployeeWageCalculation.cs
@@ -6,25 +6,24 @@ namespace EmployeCalculation
 {
     class EmployeeWageCalculation
     {
-        public const int WAGE_PR_HR = 20, FULL_DAY_HOUR = 8, HALF_DAY_HOUR = 4;
-        int totalSalary;
+        public const int WAGE_PR_HR = 20, FULL_DAY_HOUR = 8, HALF_DAY_HOUR = 4, PRESENT = 1, HALF_DAY = 2;
+        int totalSalary, TOTAL_HOUR=0;
         public void EmployeeDailyWag()
         {
             Random rand = new Random();
             int empCheck = rand.Next(0, 3);
-            if (empCheck == 1)
+            switch (empCheck)
             {
-                this.totalSalary = WAGE_PR_HR * FULL_DAY_HOUR;
-                Console.WriteLine("Employee is Present and Salary is: " + this.totalSalary);
+                case PRESENT: this.TOTAL_HOUR = FULL_DAY_HOUR;
+                    break;
+                case HALF_DAY: this.TOTAL_HOUR = HALF_DAY_HOUR;
+                    break;
+                default:
+                    Console.WriteLine("Employee is Absent for the Day");
+                    break;
+                    this.totalSalary = WAGE_PR_HR * TOTAL_HOUR;
+                    Console.WriteLine("Employee Salary is: " + totalSalary);
             }
-            if (empCheck == 2) 
-            {
-                this.totalSalary = WAGE_PR_HR * HALF_DAY_HOUR;
-                Console.WriteLine("Employee is Present for Half Day and Salary is: " + this.totalSalary);
-            }
-
-            else
-                Console.WriteLine("Employee is Absent");
         }
     }
 }
