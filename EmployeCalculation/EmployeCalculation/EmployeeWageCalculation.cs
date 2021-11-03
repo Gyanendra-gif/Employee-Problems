682caa57dd79aca3e2c24575db19611e6bd5fd88
Gyanendra Pratap Singh Sengar <[gyanendra1252@gmail.com]>
2021-11-03 14:10:12 +0530

    [Gyanendra] Add: Program to check Emp. Wage

--- EmployeCalculation/EmployeCalculation/EmployeeWageCalculation.cs
+++ EmployeCalculation/EmployeCalculation/EmployeeWageCalculation.cs
@@ -6,13 +6,17 @@ namespace EmployeCalculation
 {
     class EmployeeWageCalculation
     {
-        const int Present = 1;
-        public void EmployeeCheck()
+        public const int WAGE_PR_HR = 20, FULL_DAY_HOUR = 8;
+        int totalSalary;
+        public void EmployeeDailyWag()
         {
             Random rand = new Random();
             int empCheck = rand.Next(0, 2);
             if (empCheck == 1)
-                Console.WriteLine("Employee is Present");
+            {
+                this.totalSalary = WAGE_PR_HR * FULL_DAY_HOUR;
+                Console.WriteLine("Employee is Present and Salary is: " + this.totalSalary);
+            }
             else
                 Console.WriteLine("Employee is Absent");
         }
