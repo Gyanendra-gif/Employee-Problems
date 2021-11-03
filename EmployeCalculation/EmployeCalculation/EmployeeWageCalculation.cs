3c75bcd1beeb375559dd704c93d79e27b630f3d0
Gyanendra Pratap Singh Sengar <[gyanendra1252@gmail.com]>
2021-11-03 14:28:31 +0530

    [Gyanendra] Add: Program to check Emp. Wage as Part Time also

--- EmployeCalculation/EmployeCalculation/EmployeeWageCalculation.cs
+++ EmployeCalculation/EmployeCalculation/EmployeeWageCalculation.cs
@@ -6,17 +6,23 @@ namespace EmployeCalculation
 {
     class EmployeeWageCalculation
     {
-        public const int WAGE_PR_HR = 20, FULL_DAY_HOUR = 8;
+        public const int WAGE_PR_HR = 20, FULL_DAY_HOUR = 8, HALF_DAY_HOUR = 4;
         int totalSalary;
         public void EmployeeDailyWag()
         {
             Random rand = new Random();
-            int empCheck = rand.Next(0, 2);
+            int empCheck = rand.Next(0, 3);
             if (empCheck == 1)
             {
                 this.totalSalary = WAGE_PR_HR * FULL_DAY_HOUR;
                 Console.WriteLine("Employee is Present and Salary is: " + this.totalSalary);
             }
+            if (empCheck == 2) 
+            {
+                this.totalSalary = WAGE_PR_HR * HALF_DAY_HOUR;
+                Console.WriteLine("Employee is Present for Half Day and Salary is: " + this.totalSalary);
+            }
+
             else
                 Console.WriteLine("Employee is Absent");
         }
