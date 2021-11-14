6c75e0fdec7d8afe78c6d6750e8608e61801147a
Gyanendra Pratap Singh Sengar <[gyanendra1252@gmail.com]>
2021-11-14 08:29:38 +0530

    [Gyanendra] Add: A Program to Calculate Wage by Refactor Method

--- EmployeCalculation/EmployeCalculation/EmployeeWageCalculation.cs
+++ EmployeCalculation/EmployeCalculation/EmployeeWageCalculation.cs
@@ -7,12 +7,14 @@ namespace EmployeCalculation
     class EmployeeWageCalculation
     {
         public const int WAGE_PR_HR = 20, FULL_DAY_HOUR = 8, HALF_DAY_HOUR = 4, PRESENT = 1, HALF_DAY = 2, TOTAL_DAYS = 20, TOTAL_HOUR = 100;
-        int totalSalary, empHr;
+        int totalSalary, empHr, totalEmpHrs=0, totalWorkingDays=0;
         public void MonthlyEmployeeWage()
         {
             Random rand = new Random();
             int empCheck = rand.Next(0, 3);
-            for (int i = 0; i < totalDays && i < totalHr ; i++)
+            while (totalEmpHrs <= TOTAL_HOUR && totalWorkingDays <= TOTAL_DAYS)
+            {
+                totalWorkingDays++;
                 switch (empCheck)
                 {
                     case PRESENT:
@@ -25,8 +27,13 @@ namespace EmployeCalculation
                         Console.WriteLine("Employee is Absent for the Day");
                         break;
                 }
-            this.totalSalary = WAGE_PR_HR * empHr;
-            Console.WriteLine("Here is the Monthly Salary of Emp." + this.totalSalary);
+                totalEmpHrs += empHr;
+                Console.WriteLine( "Day#:" + totalWorkingDays + "Emp Hrs : " + empHr);
+
+            }
+            this.totalSalary = WAGE_PR_HR * totalEmpHrs;
+            Console.WriteLine("Here is the Monthly Wage of Emp." + this.totalSalary);
+
         }
     }
 }
